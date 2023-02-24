e65209b4236b1299298c9e17975fd44546055a55
RudixMasha <116712267+RudixMasha@users.noreply.github.com>
2023-02-24 17:42:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,40 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  for (int i = 2; i <= value/2; i++) {
+    if ( (value % i) == 0 ) {
+      return false;
+    } else {
+return true;
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+ uint64_t nNum = 1;
+ uint64_t res = 2;
+  while (nNum != n) {
+    res++;
+    if (checkPrime(res) == true)
+      nNum++;
+  }
+        return res;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+ bool cont = false;
+        while (cont == false) {
+                value++;
+                cont = checkPrime(value);
+        }
+        return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+ uint64_t res = 0;
+  for (int i = 0; i < hbound; i++) {
+    if (checkPrime(i)) {
+      res += i;
+    }
+  }
+  return res - 1;
 }
